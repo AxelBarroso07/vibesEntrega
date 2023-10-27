a687791bb3d99b2095f35f16a4d3105e8472795a
niconico-ca <cabralnicolas.work.email@gmail.com>
2023-10-27 20:16:23 -0300

    crud hecho (sin estilizar)

---
+++ backend/crud_productos.php
@@ -0,0 +1,127 @@
+<?php
+include('db/conexion_db.php');
+include("redimensionarImg.php");
+
+if (isset($_POST['registrar'])) {
+    $nombre = $_POST['nombre'];
+    $precio = $_POST['precio'];
+    $cantidad_stock = $_POST['cantidad_stock'];
+    if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+        move_uploaded_file($_FILES['imagen']['tmp_name'], $_FILES['imagen']['name']);
+        $imagen = redimensionarImg($_FILES['imagen']['name'], 200, 200);
+        unlink($_FILES['imagen']['name']);
+    }
+
+    $sql_insert = "INSERT INTO productos (imagen, nombre, precio, cantidad_stock) VALUES ('$imagen', '$nombre', '$precio', '$cantidad_stock')";
+    $insertar = mysqli_query($conexion, $sql_insert);
+    print("<script> window.location='crud_productos.php' </script>");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Productos - VibesAdmin</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
+</head>
+
+<body>
+    <h1>CRUD - PRODUCTOS</h1>
+    <h2>Productos listados</h2>
+    <?php
+    $sql_select = "SELECT * FROM productos";
+    $consulta = mysqli_query($conexion, $sql_select);
+    if (mysqli_num_rows($consulta) == 0) {
+        echo 'No hay productos registrados';
+    } else {
+        while ($registro = mysqli_fetch_assoc($consulta)) {
+            echo '
+                <div class="producto">
+                    <img src="../img/productos/' . $registro['imagen'] . '" alt="producto">
+                    <div class="datos">
+                        <p>Nombre: ' . $registro['nombre'] . '</p>
+                        <p>Precio: ' . $registro['precio'] . '</p>
+                        <p>Stock: ' . $registro['cantidad_stock'] . '</p>
+                        <div class="buttons">
+                            <a href="crud_productos.php?id_editar=' . $registro['id_producto'] . '&&nombre=' . $registro['nombre'] . '&&precio=' . $registro['precio'] . '&&cantidad_stock=' . $registro['cantidad_stock'].'"><button><i class="fa-solid fa-pen-to-square"></i></button></a>
+                            <a href="crud_productos.php?id_eliminar=' . $registro['id_producto'] . '"><button><i class="fa-solid fa-trash"></i></button></a>
+                        </div>
+                    </div> 
+                </div>';
+        }
+    }
+    ?>
+
+    <h2>Agregar productos</h2>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="1">Nombre: </label>
+        <input type="text" name="nombre" id="1">
+        <label for="2">Precio: </label>
+        <input type="number" name="precio" id="2">
+        <label for="3">Cantidad de stock: </label>
+        <input type="number" name="cantidad_stock" id="3">
+        <label for="4">Imagen del producto: </label>
+        <input type="file" name="imagen" id="4" accept="image/*" required>
+        <input type="submit" value="Subir nuevo producto" name="registrar">
+    </form>
+
+    <?php
+    if (isset($_GET['id_editar'])) {
+        $id_editar = $_GET['id_editar'];
+        $sql_selectEditado = "SELECT * FROM productos WHERE id_producto='$id_editar'";
+        $consulta_e = mysqli_query($conexion, $sql_selectEditado);
+        $registro_e = mysqli_fetch_assoc($consulta_e);
+        echo '<h2>EDITAR PRODUCTO: ' . $registro_e['nombre'] . '</h2>';
+        echo '<form action="" method="post" enctype="multipart/form-data">
+        <label for="1_e">Nombre: </label>
+        <input type="text" name="nombre" id="1_e">
+        <label for="2_e">Precio: </label>
+        <input type="number" name="precio" id="2_e">
+        <label for="3_e">Cantidad de stock: </label>
+        <input type="number" name="cantidad_stock" id="3_e">
+        <label for="4_e">Imagen del producto: </label>
+        <input type="file" name="imagen" id="4_e" accept="image/*">
+        <input type="hidden" name="foto_previa" value="' . $registro_e['imagen'] . '">
+        <input type="submit" value="Actualizar producto" name="actualizar">
+        </form>';
+    }
+    
+    if (isset($_POST['actualizar'])) {
+        $nombre_editar = $_POST['nombre'];
+        $precio_editar = $_POST['precio'];
+        $cantidad_editar = $_POST['cantidad_stock'];
+        $foto_previa = $_POST['foto_previa'];
+    
+        if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+            move_uploaded_file($_FILES['imagen']['tmp_name'], $_FILES['imagen']['name']);
+            $imagen = redimensionarImg($_FILES['imagen']['name'], 200, 200);
+            unlink('../img/productos/' . $foto_previa);
+        } else {
+            $imagen = $foto_previa;
+        }
+    
+        $sql_update = "UPDATE productos SET nombre='$nombre_editar', precio='$precio_editar', cantidad_stock='$cantidad_editar', imagen='$imagen' WHERE id_producto='$id_editar'";
+        $actualizar = mysqli_query($conexion, $sql_update) ? print("<script>alert('Producto editado');window.location='crud_productos.php'</script>"):print("<script>alert('Error al editar');window.location='crud_productos.php'</script>");
+    }
+    
+    if (isset($_GET['id_eliminar'])) {
+        $id_eliminar = $_GET['id_eliminar'];
+       
+        $sql_selectBorrar = "SELECT * FROM productos WHERE id_producto='$id_eliminar'";
+        $consulta_eliminare = mysqli_query($conexion, $sql_selectBorrar);
+        $registro_eliminare = mysqli_fetch_assoc($consulta_eliminare);
+     
+        unlink('../img/productos/'. $registro_eliminare['imagen']);
+        // BORRAR LA FOTO DE LA CARPETA IMAGENES
+    
+        
+        $sql_borrar = "DELETE FROM productos WHERE id_producto='$id_eliminar'";
+        $eliminar = mysqli_query($conexion, $sql_borrar) ? print("<script>alert('Producto eliminado');window.location='crud_productos.php'</script>"):print("<script>alert('Error al borrar');window.location='crud_productos.php'</script>");
+    }
+    ?>
+</body>
+
+</html>
--- backend/db/vibesdb.sql
+++ backend/db/vibesdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2023 a las 01:20:36
+-- Tiempo de generación: 28-10-2023 a las 01:13:32
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(4) NOT NULL,
+  `nombre` varchar(32) NOT NULL,
+  `precio` float NOT NULL,
+  `cantidad_stock` int(16) NOT NULL,
+  `imagen` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -37,30 +51,14 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`ID_u`, `Nbr_u`, `Img_u`, `Pass_u`, `email`, `token`) VALUES
-(1, 'axel', '', '$2y$10$trUKc/9XlRrfLT38Nltpl.7jZixNiUDTDQXPk30RhEqIk7GxE4OTC', 'axelleonelbarroso@gmail.com', 1695856174),
-(2, 'axel', '', '$2y$10$kFWI58Crixx5O3nZMHGjRenaRdmkQmTW3IAaRrPs/qzH/SJWjuoHO', 'axelleonelbarroso123@gmail.com', 1695856207),
-(3, 'axel', '', '$2y$10$r13eL/06XTv0YnJoQqn8sOcfpMiFa1S4.yAQ1p0Ga828nr2kRQHya', 'axelleonelbarroso@gmail.com', 0),
-(4, 'nico', '', '$2y$10$gbAjPjrlwAm4pMXnChNxpuG6QPcqgf/MADtRyW.6mHCJ78mLqHlNu', 'axelleonelbarroso@gmail.com', 1695856633),
-(5, 'axel', '', '$2y$10$.j8zIAZrRQx7ViP1aqZxr.zop5Cl0s.cUsWDozjsJXpCPN/E/VVTO', 'axelleonelbarroso@gmail.com', 0),
-(6, 'axel', '', '$2y$10$Zrkpy9wKFb4F62T1XyUdaOa.1d85yjsGenwvVwq0EyfH8Nc7qTPGe', 'axelleonelbarroso@gmail.com', 1696283767),
-(7, 'axel', '', '$2y$10$RTlS/Vk2YpaGVyJVJzwbFuvWXfkrZB.jhJeiBevpwHPfB/pzXcWTy', 'axelleonelbarroso@gmail.com', 1696284010),
-(8, 'axel', '', '$2y$10$gm/rFh3uZNyo05oddK9wI.8.81y4dUyYYN86IzKITkvClMVcTkVoS', 'axelleonelbarroso@gmail.com', 1696284113),
-(9, 'axel', '', '$2y$10$3OsFiWJMorN0WIH4Gb/Oae3CkSfPVX7J5Tg92IFLvin7UFnTOTyjm', 'axelleonelbarroso@gmail.com', 1696284471),
-(10, 'nico', '', '$2y$10$krqwkH3c2ncqvWtIhBHHSe9zSJMEsp8MtUCOilYAmeJ64EPKBZ75q', 'axelleonelbarroso@gmail.com', 1696284585),
-(11, 'axel', '', '$2y$10$j0bgnSRKX2T5gQpv6P/xIOWMuJBUcfvthMlRLQIqDdXnOzfHm8zFC', 'axelleonelbarroso@gmail.com', 0),
-(12, 'axel', '', '$2y$10$/8hsbbZKUkTTmLkTKYePxePZtb5X.jnHZXwRuTIUU9hgxILerJAHC', 'axelleonelbarroso@gmail.com', 1696284840),
-(13, 'axel', '', '$2y$10$bMAzZfT2DfCBwyvmEPA.hOQmTO1iFHCePZtP41NRhca0uGXOnefBi', 'axelleonelbarroso@gmail.com', 1696284946),
-(14, 'axel', '', '$2y$10$AEiGFSBlFDe1sqSaWDLJm.gauliQoEdYUBH6twvNuuz9Q9mbaS6ku', 'axelleonelbarroso@gmail.com', 0),
-(15, 'axel', '', '$2y$10$7ix8tvHzPtXZm5Pv.TPqNeD3u.Kteuq3BkSf2CC0mO9trBa0edKKu', 'axelleonelbarroso@gmail.com', 0),
-(16, 'axel', '', '$2y$10$vAgknmjdYjeFQM10KktleeMeHT7iL5k9tFeuHNIWgLw7le5LYdkUu', 'axelleonelbarroso@gmail.com', 1);
-
---
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -73,10 +71,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `ID_u` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `ID_u` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
