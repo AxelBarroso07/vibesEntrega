26516c88b03df8ada820ea50d2adc18d13406abb
niconico-ca <cabralnicolas.work.email@gmail.com>
2023-10-29 17:54:57 -0300

    index con productos

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<?php
+include('backend/db/conexion_db.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +15,7 @@
     <link rel="stylesheet" href="css/style-index.css">
     <title>Document</title>
 </head>
+
 <body>
 
 
@@ -19,19 +25,37 @@
         <?php include('./includes/header.php'); ?>
     </header>
     <section id="home" class="contenido">
-       
+
     </section>
 
     <section id="productos" class="productos">
-        
+        <?php
+        $sql_select = "SELECT * FROM productos";
+        $consulta = mysqli_query($conexion, $sql_select);
+        if (mysqli_num_rows($consulta) == 0) {
+            echo 'No hay productos registrados';
+        } else {
+            while ($registro = mysqli_fetch_assoc($consulta)) {
+                echo '
+                         <div class="container">
+                            <div>
+                             <img src="img/productos/' . $registro['imagen'] . '" alt="producto">
+                                 <p>Nombre: ' . $registro['nombre'] . '</p>
+                                 <p>Precio: ' . $registro['precio'] . '</p>
+                                 <p>Stock: ' . $registro['cantidad_stock'] . '</p>
+                            </div>
+                         </div>';
+            }
+        }
+        ?>
     </section>
-    
+
     <section id="contacto" class="contacto">
-    <?php include('./includes/contacto.html'); ?>
+        <?php include('./includes/contacto.html'); ?>
     </section>
 
 
     <script src="script.js"></script>
 </body>
-</html>
 
+</html>
