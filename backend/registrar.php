2136477d95ee02d5786bd287fa2c0e62ab6b24df
lucianalol <cabrera.luciana@tecnica7.edu.ar>
2023-10-30 11:40:35 -0300

    diseño de pantallas blancas

--- backend/registrar.php
+++ backend/registrar.php
@@ -7,14 +7,39 @@ include 'db/conexion_db.php';
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<!-- <link rel="stylesheet" href="../vibesEntrega/css/style-registrar.css"> -->
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style>
+    *{
+        background-color:black;
+   }
+    .texto {
+        /* background-color: #4CAF50;  */
+        color: white;
+        font-weight:bold; 
+        padding: 10px; 
+        text-align: center; 
+        font-size: 20px;
+        margin:250px;
+    }
+    .svg{
+        /* background-color:white; */
+        cursor:pointer;
+    }
+</style>    
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
+<!-- <div class="mensaje-enviado">Correo enviado, por favor valide</div> -->
+    <a href="../index.php">
+        <div class="svg">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+    </div>
+    </a>
+    
     <?php
 
     if (isset($_POST['registrar'])) {
@@ -22,7 +47,7 @@ include 'db/conexion_db.php';
         $emailQuery = "Select * from usuarios where email = \"$email\"";
         $emailInUse = mysqli_num_rows(mysqli_query($conexion, $emailQuery));
         if ($emailInUse >= 1) {
-            echo "Email ya en uso, utilize otro. <a href='../form_registro.html'>Reiniciar</a>";
+            echo '<p class="texto">Email ya en uso, utilize otro. <a href="../form_registro.html">Reiniciar</a></p>';
         } else {
             $Nbr_u = $_POST['usuario'];
             $contrasenia = $_POST['contrasenia'];
@@ -31,7 +56,7 @@ include 'db/conexion_db.php';
             $Pass_u = password_hash($contrasenia, PASSWORD_DEFAULT);
 
             $sql = "INSERT INTO usuarios (Nbr_u, Pass_u, email, token) VALUES ('$Nbr_u', '$Pass_u', '$email', '$token')";
-            $insertar = mysqli_query($conexion, $sql) ? print('ok') : print('error');
+            $insertar = mysqli_query($conexion, $sql);
 
 
     ?>
@@ -60,9 +85,9 @@ include 'db/conexion_db.php';
 
     if (isset($_GET['send'])) {
         if ($_GET['send'] == 1) {
-            echo 'Correo enviado, por favor valide';
+            echo '<p class="texto">Correo enviado, por favor valide</p>';
         } else {
-            echo 'error al enviar el correro de validacion';
+            echo '<p class="texto">error al enviar el correro de validacion</p>';
         }
     }
     if (isset($_GET['token'])) {
