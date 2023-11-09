1b04405e34b66869b2b4f646eb831e9e5cb82b68
lucianalol <cabrera.luciana@tecnica7.edu.ar>
2023-11-09 17:12:19 -0300

    actualizaciones con css

--- backend/perfil.php
+++ backend/perfil.php
@@ -29,21 +29,43 @@ if (isset($_SESSION['usuario'])) {
 
     <div class="columna">
         <img src="../img/fondoregistro.jpg" alt="Tu imagen" class="imagen">
-        <div class="texto">
-            <h1>BIENVENIDA/O A VIBES:</h1>
-            <hr>
-            <h2 class="nombre"> Nombre de usuario: <?php echo $nombre; ?></h2>
-            <h2 class="nombre"> Correo: <?php echo $email; ?></h2>
-        </div>
 
+        <div class="titulo">
+            <h1>BIENVENIDA/O A VIBES:</h1>
+        </div>
         <div class="texto">
+            <h2>Datos Personales</h2>
+            <hr>
+            <h3 class="nombre"> Username: <?php echo $nombre; ?></h3>
+            <h3 class="nombre"> Mail: <?php echo $email; ?></h3>
+
+        <div class="pedidos">
             <h1>Mis pedidos:</h1>
             <hr>
         </div>
+        </div>
+
+        
     </div>
 
     <section id="contacto" class="contacto">
         <?php include('../includes/contacto.html'); ?>
+    <div class="formulario">
+        <form action="" target="_blank">
+                <fieldset>
+                <h3>Información personal</h3>
+
+                <hr>
+
+                <label for="nombre">Nombre: </label><input type="text" name="nombre" id="nombre"><br><br>
+                <label>Mail: </label><input type="text" name="mail"><br><br>
+
+                <br><input type="submit" value="Enviar información"><br><br>
+                </fieldset>
+
+            </form>
+    </div>
+    
     </section>
 </body>
 
