2507aeccf9b22f3c978b4238b2230730150678bb
niconico-ca <cabralnicolas.work.email@gmail.com>
2023-11-28 13:29:51 -0300

    Finalizar compra

--- backend/includes/functionCart.php
+++ backend/includes/functionCart.php
@@ -21,7 +21,7 @@ function mostrarProductos(){
         echo '<div class="carrito">';
         foreach ($carrito as $indice => $producto) {
             echo '<div class="producto">
-                    <p class="eliminar"><a href="carrito.php?id_borrar='.$producto['ID_prod'].'"><i class="fa-solid fa-trash"></i></a></p>
+            <a href="carrito.php?id_borrar='.$producto['ID_prod'].'"><p class="eliminar"><i class="fa-solid fa-trash"></i></p></a>
                     <img src="../img/productos/'.$producto['img_prod'].'" alt="">
                     <p>'.$producto['nbr_prod'].'</p>
                     <p>Cantidad:<a href="carrito.php?id_restar='.$producto['ID_prod'].'"><br><i class="fa-regular fa-square-minus"></i></a> | '.$producto['cantidad'].' | <a href="carrito.php?id_sumar='.$producto['ID_prod'].'"><i class="fa-regular fa-square-plus"></i></a> </p>
@@ -34,7 +34,7 @@ function mostrarProductos(){
             <a href="carrito.php?finCompra" class="comprar" onClick="return confirm(\'Seguro desea proceder a comprar\')">Finalizar Compra</a>
             <div class="link">
                 <a href="carrito.php?vaciarCarrito">Vaciar Carrito</a>
-                <a href="../index.php">Seguir Comprando</a>
+                <a href="Productos.php">Seguir Comprando</a>
             </div>
         </div>';
     }
@@ -43,7 +43,7 @@ function mostrarCarritoVacio(){
     echo '
     <div class="vacio">
     <div class="error">Carrito vacio</div>
-    <a class="ir-tienda" href="../index.php">Ir a Tienda</a>
+    <a class="ir-tienda" href="Productos.php">Ir a Tienda</a>
     </div>';
 }
 
@@ -136,6 +136,104 @@ function restarCantProd($id_restar){
                 borrarProdCarrito($id_restar);
             }
         }   
+    }
+}
+
+
+/*implode($array) = convierte un array es un string)*/
+/*explode('separador', $string) = convierte un string en un array, utilizando el separador elegido */
+
+function finalizarCompra(){
+    include('db/conexion_db.php');
+    $carrito = $_SESSION['carrito'];
+
+    /*Recorremos el carrito para generar el array de id_prod/precio/cant y para actualizar la disponibilidad de los productos */
+    foreach($carrito as $indice => $producto){
+        /*genero un vector $order con ID_prod/precio/cantidad separado prod a prod por un espacio en blanco*/
+        $order[] = ' '.$producto['ID_prod'].'/'.$producto['precio_prod'].'/'.$producto['cantidad'];
+        /*el espacio en blanco adelante hace que $order[0] quede vacio*/
+
+        /* Actualizo la disponibilidad de cada producto */
+        $id_prod = $producto['ID_prod'];
+        $nuevo_stock = $producto['stock'] - $producto['cantidad'];
+        $sql_update = "UPDATE productos SET cantidad_stock = '$nuevo_stock' WHERE id_producto = '$id_prod'";
+        $update_stock = mysqli_query($conexion, $sql_update);
+    }
+
+    // veo/reviso que los datos del array $order sean los correctos
+    // echo '<pre>';
+    // print_r($order);
+    // echo '</pre>';
+  
+    /*Asigno el valor a las variable que utilizare para grabar el registro */
+    $fecha = date("Y/m/d");
+    $productos = implode($order); //convierto el array $order en un string para guardarlo en la db
+    $id_usuario = $_SESSION['ID_user'];
+
+    /*Guardo el registro de orden el la tabal 'orders' */
+    $sql = "INSERT INTO orders (Date_order, articles, ID_user) VALUES ('$fecha','$productos','$id_usuario')";
+    $insertar = mysqli_query($conexion, $sql);
+
+    /*Si se crea el registro, elimino la variable de session, con lo cual vacio el carrito y muestro mensaje */
+    if($insertar){
+        unset($_SESSION['carrito']);
+        print ('<script>alert("Pedido Generado Exitosamente")</script>');
+    }else{
+        print ('<script>alert("Error al Generar Pedido")</script>');
+    }
+}
+
+
+function mostrarPedidos($ID_user){
+    include('db/conexion_db.php');
+
+    $sql = "SELECT * FROM orders WHERE ID_user = '$ID_user' ORDER BY Date_order DESC";
+    $consulta = mysqli_query($conexion, $sql);
+
+    if(mysqli_num_rows($consulta)>0){
+        while($registro=mysqli_fetch_assoc($consulta)){
+            /* muestro Nro de orden y Fecha */
+            echo '<details>
+            <summary>Nro. de Orden: '.$registro['ID_order']. ' | Fecha: '.$registro['Date_order'].'</summary>';
+            
+
+            /*convierto el campor Article nuevamente en un vector utilizando la funcion explode() */
+            $articulos = explode(' ' , $registro['articles']);
+
+            $total = 0;
+            for($x=1; $x<count($articulos); $x++){
+                    /*utilizando nuevamente la funcion explote() generos las variables para guardar el id_prod, el precio y la cantidad */
+                    list($id, $precio, $cant) = explode('/' , $articulos[$x]);
+
+                    /*Con el $id traigo imagen y nombre de producto */
+                    $sql2 = "SELECT nombre, imagen FROM productos WHERE id_producto='$id'";
+                    $consulta2 = mysqli_query($conexion, $sql2);
+                    $reg_art = mysqli_fetch_assoc($consulta2);
+
+                    /* muestro detalle de pedido */
+                    echo '
+                    <div class="detalle">
+                        <div class="img"> <img src="../img/productos/'.$reg_art['imagen'].'" alt="producto"></div>
+                        <div class="datos">
+                            <span>'.$reg_art['nombre'].'</span>
+                            <span> $'.number_format($precio,2,",",".").'</span>
+                            <span> Cantidad:'.$cant.'</span>
+                            <span> Subtotal: $'.number_format($precio*$cant,2,",",".").'</span>
+                        </div>
+                    </div>
+                    '; 
+                    $total = $total + ($precio*$cant);     
+            }
+            echo '
+            <div class="total">
+            <span> TOTAL: $'.number_format($total,2,",",".").'</span>
+            </div>
+            ';
+            echo '</details>';
+           
+        }
+    }else{
+        echo 'No tiene Pedidos';
     }
 }
 
--- backend/ver_producto.php
+++ backend/ver_producto.php
@@ -21,17 +21,80 @@ $consulta = mysqli_query($conexion, $sql_select);
         <?php
         while ($registro = mysqli_fetch_assoc($consulta)) {
             echo '<div class="producto">
-            <div class="image"><img src="../img/productos/'. $registro['imagen'] .'></div>
+            <div class="image"><img src="../img/productos/'. $registro['imagen'] .'" alt="producto"></div>
                     <div class="descripcion">
                         <p class="nombre">' . $registro['nombre'] . '</p>
                         <p class="price">Precio: ' . $registro['precio'] . '</p>
                         <p class="talles">Talles: ' . $registro['talles'] . '</p>
                     </div>
                     <div class="buttons">
-                        <button class="editar">Editar</button>
-                        <button class="eliminar">Eliminar</button>
+                    <a href="ver_producto.php?id_editar=' . $registro['id_producto'] . '&&nombre=' . $registro['nombre'] . '&&precio=' . $registro['precio'] . '&&cantidad_stock=' . $registro['cantidad_stock'] . '&&talles=' . $registro['talles'].'"><button class="editar">Editar</button></a>
+                    <a href="ver_producto.php?id_eliminar=' . $registro['id_producto'] . '"><button class="eliminar">Eliminar</button></a>
                     </div>
                   </div>';
+        }
+
+        if (isset($_GET['id_editar'])) {
+            $id_editar = $_GET['id_editar'];
+            $sql_selectEditado = "SELECT * FROM productos WHERE id_producto='$id_editar'";
+            $consulta_e = mysqli_query($conexion, $sql_selectEditado);
+            $registro_e = mysqli_fetch_assoc($consulta_e);
+            echo '<h2>EDITAR PRODUCTO: ' . $registro_e['nombre'] . '</h2>';
+            echo '<form action="" method="post" enctype="multipart/form-data">
+            <label for="nombre_e">Nombre: </label>
+            <input type="text" name="nombre" id="nombre_e" value="' . $registro_e['nombre'] . '">
+            <label for="precio_e">Precio: </label>
+            <input type="number" name="precio" id="precio_e" value="' . $registro_e['precio'] . '">
+            <label for="cantidad_stock_e">Cantidad de stock: </label>
+            <input type="number" name="cantidad_stock" id="cantidad_stock_e" value="' . $registro_e['cantidad_stock'] . '">
+            <label for="informacion_e">Información: </label>
+            <textarea name="informacion" id="informacion_e" rows="4">' . $registro_e['informacion'] . '</textarea>
+            <label for="talles_e">Talles: </label>
+            <select name="talles" id="talles_e">
+                <option value="S" ' . ($registro_e['talles'] == 'S' ? 'selected' : '') . '>S</option>
+                <option value="M" ' . ($registro_e['talles'] == 'M' ? 'selected' : '') . '>M</option>
+                <option value="L" ' . ($registro_e['talles'] == 'L' ? 'selected' : '') . '>L</option>
+                <option value="XL" ' . ($registro_e['talles'] == 'XL' ? 'selected' : '') . '>XL</option>
+            </select>
+            <label for="imagen_e">Imagen del producto: </label>
+            <input type="file" name="imagen" id="imagen_e" accept="image/*">
+            <input type="hidden" name="foto_previa" value="' . $registro_e['imagen'] . '">
+            <input type="submit" value="Actualizar producto" name="actualizar">
+            </form>';
+        }
+    
+        if (isset($_POST['actualizar'])) {
+            $nombre_editar = $_POST['nombre'];
+            $precio_editar = $_POST['precio'];
+            $cantidad_editar = $_POST['cantidad_stock'];
+            $informacion_editar = $_POST['informacion'];
+            $tallas_editar = $_POST['talles'];
+            $foto_previa = $_POST['foto_previa'];
+    
+            if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+                move_uploaded_file($_FILES['imagen']['tmp_name'], $_FILES['imagen']['name']);
+                $imagen = redimensionarImg($_FILES['imagen']['name'], 200, 200);
+                unlink('../img/productos/' . $foto_previa);
+            } else {
+                $imagen = $foto_previa;
+            }
+    
+            $sql_update = "UPDATE productos SET nombre='$nombre_editar', precio='$precio_editar', cantidad_stock='$cantidad_editar', informacion='$informacion_editar', talles='$tallas_editar', imagen='$imagen' WHERE id_producto='$id_editar'";
+            $actualizar = mysqli_query($conexion, $sql_update) ? print("<script>alert('Producto editado');window.location='ver_producto.php'</script>") : print("<script>alert('Error al editar');window.location='ver_producto.php'</script>");
+        }
+    
+        if (isset($_GET['id_eliminar'])) {
+            $id_eliminar = $_GET['id_eliminar'];
+    
+            $sql_selectBorrar = "SELECT * FROM productos WHERE id_producto='$id_eliminar'";
+            $consulta_eliminare = mysqli_query($conexion, $sql_selectBorrar);
+            $registro_eliminare = mysqli_fetch_assoc($consulta_eliminare);
+    
+            unlink('../img/productos/' . $registro_eliminare['imagen']);
+            // BORRAR LA FOTO DE LA CARPETA IMAGENES
+    
+            $sql_borrar = "DELETE FROM productos WHERE id_producto='$id_eliminar'";
+            $eliminar = mysqli_query($conexion, $sql_borrar) ? print("<script>alert('Producto eliminado');window.location='ver_producto.php'</script>") : print("<script>alert('Error al borrar');window.location='ver_producto.php'</script>");
         }
         ?>
     </section>
