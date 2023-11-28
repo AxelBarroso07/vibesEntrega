75d79c004e2f18a628236afa59b5d61ac3a977f1
niconico-ca <cabralnicolas.work.email@gmail.com>
2023-11-28 20:52:16 -0300

    archivos historial agregado

---
+++ backend/ventas_admin.php
@@ -0,0 +1,28 @@
+<?php
+include('db/conexion_db.php');
+include("redimensionarImg.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historial de Ventas</title>
+
+</head>
+
+<body>
+
+    <section class="general">
+    <div class="button-back">
+               <a href="vibesAdmin.php">Volver</a> 
+    </div>
+            <h1 class="title">Historial de Ventas</h1>
+            
+    </section>
+
+</body>
+
+</html>
